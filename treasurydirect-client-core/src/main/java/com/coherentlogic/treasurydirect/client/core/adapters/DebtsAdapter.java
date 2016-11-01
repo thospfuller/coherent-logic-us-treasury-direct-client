c19d593744bf33ca1d8ebdc6bbb70419023e991e
thospfuller <thomas.fuller@coherentlogic.com>
2016-11-01 12:40:45 -0400

    Implementation details have been added.

--- treasurydirect-client-core/src/main/java/com/coherentlogic/treasurydirect/client/core/adapters/DebtsAdapter.java
+++ treasurydirect-client-core/src/main/java/com/coherentlogic/treasurydirect/client/core/adapters/DebtsAdapter.java
@@ -27,7 +27,13 @@ public class DebtsAdapter extends AbstractGSONBasedTypeAdapter<Debts> {
 
     private static final Logger log = LoggerFactory.getLogger(DebtsAdapter.class);
 
-    public static final String BEAN_NAME = "debtsAdapter";
+    public static final String BEAN_NAME = "debtsAdapter", ENTRIES = "entries";
+
+    public static final String
+        EFFECTIVE_DATE = "effectiveDate",
+        GOVERNMENT_HOLDINGS = "governmentHoldings",
+        PUBLIC_DEBT = "publicDebt",
+        TOTAL_DEBT = "totalDebt";
 
     public DebtsAdapter(TypedFactory<Debts> typedFactory) {
         super(typedFactory);
@@ -37,6 +43,39 @@ public class DebtsAdapter extends AbstractGSONBasedTypeAdapter<Debts> {
         super(gsonBuilder, typedFactory);
     }
 
+    /**
+     * Method converts debt-related JSON into an instance of Debts and returns the result.
+     * 
+     * Single debt JSON:
+     * 
+     * {
+     *     "effectiveDate":"October 28, 2016 EDT",
+     *     "governmentHoldings":5510526280153.27,
+     *     "publicDebt":14309982958438.22,
+     *     "totalDebt":19820509238591.49
+     * }
+     * 
+     * Multiple debts JSON:
+     *
+     * {
+     *     "entries":
+     *         [
+     *             {
+     *                 "effectiveDate":"January 31, 2014 EST",
+     *                 "governmentHoldings":4984876805567.80,
+     *                 "publicDebt":12308142849415.81,
+     *                 "totalDebt":17293019654983.61
+     *             },
+     *             {
+     *                 "effectiveDate":"January 30, 2014 EST",
+     *                 ...
+     *             }
+     *         ]
+     * }
+     *
+     * @see http://www.treasurydirect.gov/NP_WS/debt/current?format=json
+     * @see http://www.treasurydirect.gov/NP_WS/debt/search?startdate=2014-01-01&enddate=2014-02-01&format=json
+     */
     @Override
     public Debts read(JsonReader reader) throws IOException {
 
@@ -49,7 +88,9 @@ public class DebtsAdapter extends AbstractGSONBasedTypeAdapter<Debts> {
         JsonToken token = reader.peek ();
 
         if (JsonToken.BEGIN_ARRAY.equals(token)) {
-            debtsElement = getGsonBuilder().create().fromJson(reader, JsonArray.class);
+            throw new ConversionFailedException("Unable to convert the JSON into an instance of Debts due to "
+                + "the resultant json starting with an array token and this is not expected.");
+            //debtsElement = getGsonBuilder().create().fromJson(reader, JsonArray.class);
         } else if (JsonToken.BEGIN_OBJECT.equals(token)) {
             debtsElement = getGsonBuilder().create().fromJson(reader, JsonObject.class);
         } else {
@@ -57,24 +98,9 @@ public class DebtsAdapter extends AbstractGSONBasedTypeAdapter<Debts> {
                 + "an unsupported token type (token: " + token + ").");
         }
 
-        List<Debt> debtList = null;
+        List<Debt> debtList = new ArrayList<Debt> ();
 
-        if (debtsElement.isJsonArray()) {
-
-            JsonArray debtArray = debtsElement.getAsJsonArray();
-
-            debtList = asDebtList (debtArray);
-
-            result.setDebtList(debtList);
-
-        } else {
-
-            debtList = new ArrayList<Debt> ();
-
-            Debt debt = asDebt(debtsElement.getAsJsonObject());
-
-            debtList.add(debt);
-        }
+        asDebtList (debtsElement.getAsJsonObject(), debtList);
 
         result.setDebtList(debtList);
 
@@ -83,37 +109,171 @@ public class DebtsAdapter extends AbstractGSONBasedTypeAdapter<Debts> {
         return result;
     }
 
-    Debt asDebt (JsonObject securityObject) {
+    /**
+     * Method creates a new instance of Debt, maps the debtObject parameters to the debt object, and returns the result.
+     *
+     * Note that the debtObject should have the following format:
+     *
+     * {
+     *     "effectiveDate":"January 31, 2014 EST",
+     *     "governmentHoldings":4984876805567.80,
+     *     "publicDebt":12308142849415.81,
+     *     "totalDebt":17293019654983.61
+     * }
+     */
+    Debt asDebt (JsonObject debtObject) {
 
         Debt result = new Debt ();
 
-        String effectiveDate = securityObject.get("effectiveDate").toString();
-
-        log.debug("effectiveDate: " + effectiveDate);
-
-        result.setEffectiveDate(effectiveDate);
-
-        String governmentHoldings = securityObject.get("governmentHoldings").toString();
-
-        log.debug("governmentHoldings: " + governmentHoldings);
-
-        result.setGovernmentHoldings(governmentHoldings);
-
-        String publicDebt = securityObject.get("publicDebt").toString();
-
-        log.debug("publicDebt: " + publicDebt);
-
-        result.setPublicDebt(publicDebt);
-
-        String totalDebt = securityObject.get("totalDebt").toString();
-
-        log.debug("totalDebt: " + totalDebt);
-
-        result.setTotalDebt(totalDebt);
+        asDebt (debtObject, result);
 
         return result;
     }
 
+    /**
+     * Method maps the properties of the single debtObject, below, into the debt object.
+     *
+     * {
+     *     "effectiveDate":"January 31, 2014 EST",
+     *     "governmentHoldings":4984876805567.80,
+     *     "publicDebt":12308142849415.81,
+     *     "totalDebt":17293019654983.61
+     * }
+     */
+    void asDebt (JsonObject debtObject, Debt debt) {
+
+        String effectiveDate = debtObject.get(EFFECTIVE_DATE).toString();
+
+        log.debug("effectiveDate: " + effectiveDate);
+
+        debt.setEffectiveDate(effectiveDate);
+
+        String governmentHoldings = debtObject.get(GOVERNMENT_HOLDINGS).toString();
+
+        log.debug("governmentHoldings: " + governmentHoldings);
+
+        debt.setGovernmentHoldings(governmentHoldings);
+
+        String publicDebt = debtObject.get(PUBLIC_DEBT).toString();
+
+        log.debug("publicDebt: " + publicDebt);
+
+        debt.setPublicDebt(publicDebt);
+
+        String totalDebt = debtObject.get(TOTAL_DEBT).toString();
+
+        log.debug("totalDebt: " + totalDebt);
+
+        debt.setTotalDebt(totalDebt);
+    }
+
+    /**
+     * Method converts the debtArray, as below, into an instance of one or more Debt objects, and adds the results to
+     * the debtList.
+     *
+     * [
+     *     {
+     *         "effectiveDate":"January 31, 2014 EST",
+     *         "governmentHoldings":4984876805567.80,
+     *         "publicDebt":12308142849415.81,
+     *         "totalDebt":17293019654983.61
+     *     },
+     *     {
+     *         "effectiveDate":"January 30, 2014 EST",
+     *         ...
+     *     }
+     * ]
+     */
+    void asDebtList (JsonArray debtArray, List<Debt> debtList) {
+
+        for (JsonElement debtElement : debtArray) {
+
+            if (debtElement.isJsonObject()) { // expect objects in the entries array.
+
+                JsonObject debtObject = debtElement.getAsJsonObject();
+
+                Debt debt = asDebt (debtObject);
+
+                debtList.add(debt);
+
+            } else
+                throw new ConversionFailedException("Expected an instance of JsonObject for the debtElement: " +
+                    debtElement);
+        }
+    }
+
+    /**
+     * Method converts a list of entries of debt data OR a single instance of debt data and adds the result(s) to the
+     * debtList.
+     *
+     * For example, the debtObject may look like EITHER:
+     *
+     * {
+     *     "effectiveDate":"October 28, 2016 EDT",
+     *     "governmentHoldings":5510526280153.27,
+     *     "publicDebt":14309982958438.22,
+     *     "totalDebt":19820509238591.49
+     * }
+     *
+     * OR
+     *
+     * {
+     *     "entries":
+     *         [
+     *             {
+     *                 "effectiveDate":"January 31, 2014 EST",
+     *                 "governmentHoldings":4984876805567.80,
+     *                 "publicDebt":12308142849415.81,
+     *                 "totalDebt":17293019654983.61
+     *             },
+     *             {
+     *                 "effectiveDate":"January 30, 2014 EST",
+     *                 ...
+     *             }
+     *         ]
+     * }
+     *
+     * -- any other format is not supported.
+     */
+    void asDebtList (JsonObject debtObject, List<Debt> debtList) {
+
+        if (debtObject.has(ENTRIES)) {
+
+            JsonElement entriesElement = debtObject.get(ENTRIES);
+
+            if (entriesElement.isJsonArray()) {
+
+                JsonArray entriesArray = entriesElement.getAsJsonArray();
+
+                asDebtList(entriesArray, debtList);
+            } else
+                throw new ConversionFailedException("Expected an instance of JsonArray for the entriesElement: " +
+                    entriesElement);
+        } else {
+
+            // If there are no entries then the debtObject should be convertible to a single instance of Debt, otherwise
+            // something is amiss with the JSON and we have a problem or we may not have accounted for converting the
+            // JSON possibly.
+
+            Debt result = asDebt(debtObject);
+
+            debtList.add(result);
+        }
+    }
+
+    /**
+     * Method converts the debtArray into list of Debt objects -- for example, the debtArray should contain the
+     * following:
+     *
+     * [
+     *     {"effectiveDate":"January 31, 2014 EST",
+     *      "governmentHoldings":4984876805567.80,
+     *      "publicDebt":12308142849415.81,
+     *      "totalDebt":17293019654983.61
+     *     },
+     *     ....
+     * ]
+     */
     List<Debt> asDebtList (JsonArray debtArray) {
 
         List<Debt> result = new ArrayList<Debt> ();
